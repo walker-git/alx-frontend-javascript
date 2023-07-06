22057d697ec753252b45fc49d2f6381f573bbcf5
walker-git <setiks92@gmail.com>
2023-07-06 08:25:33 -0700

    0-get_list_students.js

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,23 @@
+function getListStudents() {
+  // Array of student objects
+  var students = [
+    {
+      id: 1,
+      firstName: 'Guillaume',
+      location: 'San Francisco'
+    },
+    {
+      id: 2,
+      firstName: 'James',
+      location: 'Columbia'
+    },
+    {
+      id: 5,
+      firstName: 'Serena',
+      location: 'San Francisco'
+    }
+  ];
+
+  return students;
+}
+
